3aa8922cd46688809938928fbb1ad0671290be46
ngoerne <51130956+nostalgique@users.noreply.github.com>
2019-07-02 18:30:30 +0100

    add search functionality (city)

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let apiKey = "9b41700e7dcfb48ddf3be545946f91cc";
-let city = "Lisbon";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+function search(city) {
+  let apiKey = "9b41700e7dcfb48ddf3be545946f91cc";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(showCurrentWeather);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let keyword = document.querySelector("#keyword");
+  search(keyword.value);
+}
 function showCurrentWeather(response) {
   let currentTemperature = document.querySelector("#currentTemperature");
   currentTemperature.innerHTML = Math.round(response.data.main.temp);
@@ -22,4 +29,5 @@ function showCurrentWeather(response) {
   maximumCurrentTemperature.innerHTML = Math.round(response.data.main.temp_max);
 }
 
-axios.get(url).then(showCurrentWeather);
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
