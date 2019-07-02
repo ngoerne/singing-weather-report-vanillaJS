42a8119a872bec25ef0f114c82bde5d66a953e04
ngoerne <51130956+nostalgique@users.noreply.github.com>
2019-07-02 15:14:52 +0100

    display current temperature

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let apiKey = "9b41700e7dcfb48ddf3be545946f91cc";
-let city = "Lisbon";
+let city = "London";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-
 function showCurrentTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  document.querySelector(".currentTemperature").innerHTML = `${temperature} °C`;
+  let currentTemperature = document.querySelector("#currentTemperature");
+  currentTemperature.innerHTML = Math.round(response.data.main.temp);
+
+  let place = document.querySelector("#place");
+  place.innerHTML = response.data.name;
 }
 
 axios.get(url).then(showCurrentTemperature);
