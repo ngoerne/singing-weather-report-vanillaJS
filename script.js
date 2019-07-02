e0bf063a60a529c099502f547079dacb16ffd50e
ngoerne <51130956+nostalgique@users.noreply.github.com>
2019-07-02 15:39:37 +0100

    display current weather (min, max, description)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let apiKey = "9b41700e7dcfb48ddf3be545946f91cc";
-let city = "London";
+let city = "Lisbon";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 function showCurrentTemperature(response) {
   let currentTemperature = document.querySelector("#currentTemperature");
@@ -7,6 +7,19 @@ function showCurrentTemperature(response) {
 
   let place = document.querySelector("#place");
   place.innerHTML = response.data.name;
+
+  let weatherDescription = document.querySelector("#weatherDescription");
+  weatherDescription.innerHTML = response.data.weather[0].description;
+
+  let minimumCurrentTemperature = document.querySelector(
+    "#minimumCurrentTemperature"
+  );
+  minimumCurrentTemperature.innerHTML = Math.round(response.data.main.temp_min);
+
+  let maximumCurrentTemperature = document.querySelector(
+    "#maximumCurrentTemperature"
+  );
+  maximumCurrentTemperature.innerHTML = Math.round(response.data.main.temp_max);
 }
 
 axios.get(url).then(showCurrentTemperature);
