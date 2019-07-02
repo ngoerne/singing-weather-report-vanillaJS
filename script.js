8b737263c8c4d8fbfde695f89081af603d4f7d11
ngoerne <51130956+nostalgique@users.noreply.github.com>
2019-07-02 18:01:54 +0100

    change function name

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let apiKey = "9b41700e7dcfb48ddf3be545946f91cc";
 let city = "Lisbon";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-function showCurrentTemperature(response) {
+function showCurrentWeather(response) {
   let currentTemperature = document.querySelector("#currentTemperature");
   currentTemperature.innerHTML = Math.round(response.data.main.temp);
 
@@ -22,4 +22,4 @@ function showCurrentTemperature(response) {
   maximumCurrentTemperature.innerHTML = Math.round(response.data.main.temp_max);
 }
 
-axios.get(url).then(showCurrentTemperature);
+axios.get(url).then(showCurrentWeather);
